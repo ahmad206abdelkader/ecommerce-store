9d4620bf9901e345ae4d64a0e8dd958ad26e53e7
a7mad <ahmad11iphone11abdelkader11@gmail.con>
2023-08-29 21:15:01 +0300

    initial commit

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,25 @@
+import Container from "@/components/ui/container";
+import Billboard from "@/components/billboard";
+import getBillboards from '@/actions/get-billboards';
+import getProducts from "@/actions/get-products";
+import ProductList from "@/components/product-list";
+
+export const revalidate = 0;
+
+
+const HomePage = async () => {
+    const products= await getProducts({isFeatured: true});
+    const billboard = await getBillboards('59e34fb6-b7b8-46e8-9bc4-537ecf37a042');
+    return (
+     <Container>
+        <div className="space-y-10 pb-10">
+            <Billboard data={billboard}/>
+        </div>
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title='Featuerd Products' items={products} />
+        </div>
+     </Container>
+    );
+}
+
+export default HomePage;
